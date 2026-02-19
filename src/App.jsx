89ea3f47a30741fa94f1b708b82056ba5fa11a58
AYUK AMELIA <ameliafertilizer123@gmail.com>
2026-02-19 13:11:19 +0100

    Fix BusinessSignUp UI and update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginSignUp from "./components/LoginSignUp/LoginSignUp";
 import BusinessSignUp from "./components/BusinessSignUp/BusinessSignUp";
